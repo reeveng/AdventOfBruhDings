4027e7adadffa58e09d3336e988257f9939ec5be
reeveng <govaertreeven@gmail.com>
2022-12-01 19:02:11 +0100

    part two, apparently there is a part two KEKL

--- day 01 - find max/dinkg.js
+++ day 01 - find max/dinkg.js
@@ -4,9 +4,19 @@ const dingk = (data) => {
     .split("\n\n")
     .map((i) => i.split(`\n`).map((i) => +i));
 
+  // part one
   console.log(
     Math.max(...convertedStuff.map((i) => i.reduce((a, b) => a + b, 0)))
   );
+
+  // part two
+  console.log(
+    convertedStuff
+      .map((i) => i.reduce((a, b) => a + b, 0))
+      .sort((a, b) => b - a)
+  );
+
+  console.log(71300 + 69249 + 69142);
 };
 
 fs.readFile("input.txt", "utf8", function (err, data) {
