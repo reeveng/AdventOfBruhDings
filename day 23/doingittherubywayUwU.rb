060429822ec8103121660c8804480eecdda0561d
reeveng <govaertreeven@gmail.com>
2022-12-30 16:29:16 +0100

    update the thing

---
+++ day 23/doingittherubywayUwU.rb
@@ -0,0 +1,15 @@
+require 'set'
+
+# .lines(chomp: true) = split on line endings (kind of)
+dings = File.read("input.txt").lines(chomp: true).filter_map{|x|
+  x
+}
+
+# # -------------------------------
+# # part 1
+# # -------------------------------
+
+# # -------------------------------
+# # part 2
+# # -------------------------------
+
