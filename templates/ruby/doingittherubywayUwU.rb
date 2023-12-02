2c7fb3161fa0e92ce655d874ea1c8e2794de4cbc
JuiceMitApfelnDrin <juicemitapfelndrin@gmail.com>
2023-12-02 20:10:08 +0100

    add another random idea

---
+++ templates/ruby/doingittherubywayUwU.rb
@@ -0,0 +1,18 @@
+require 'set'
+
+# .lines(chomp: true) = split on line endings (kind of)
+dings = File.read("input.txt").lines(chomp: true).filter_map{|x|
+  x
+}
+
+# # -------------------------------
+# # part 1
+# # -------------------------------
+
+
+
+# # -------------------------------
+# # part 2
+# # -------------------------------
+
+puts "hi"
